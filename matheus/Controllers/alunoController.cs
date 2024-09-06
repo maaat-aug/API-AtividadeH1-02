211618367b2edbae612712c5311294ffb6adeaf9
matheus.teixeira <matheus.teixeira@ZE-TI16>
2024-09-06 08:37:31 -0300

    Adicionar arquivos de projeto.

---
+++ matheus/Controllers/alunoController.cs
@@ -0,0 +1,32 @@
+﻿using matheus.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace matheus.Controllers
+{
+       
+    [ApiController]
+    public class alunoController : ControllerBase
+    {
+        public List<int> numeros = [1,2,3,4,5,6,7,8,9,0];
+
+        public static List<Pessoa> pessoas = new List<Pessoa>();
+        [HttpPost]
+        [Route("Registro Pessoas")]
+        public IActionResult RegistroAluno([FromBody] Pessoa pessoa)
+        {
+            pessoas.Add(pessoa);
+
+            const string numeros = "0123456789";
+
+           if (pessoa.RA.StartsWith("RA")) {
+
+                for (int i = 2; i < pessoa.RA.Length; i++)
+                    if (numeros.IndexOf(pessoa.RA[i]) < 0) return BadRequest("RA Invalido");
+                return Ok("Usuario registrado");
+            }
+            return BadRequest("RA Invalido");
+            }
+        }
+    }
+
---
+++ matheus/Models/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+
+namespace matheus.Models
+{
+    public class Pessoa
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(3)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        [MinLength(11)]
+        public string CPF { get; set; }
+
+        [Required]
+        public bool Ativo {  get; set; }
+
+        [MaxLength(8)]
+        [Required]
+       // [RAvalidation]
+
+        public string RA {  get; set; }
+
+    }
+}
